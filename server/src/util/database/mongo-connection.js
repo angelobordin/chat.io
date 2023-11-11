06206e355c6ad126c3322afb90f363e84cd30da9
AngeloBordin <angelombordin@gmail.com>
2023-11-11 16:50:45 -0300

    refactor classes and functions

--- server/src/util/database/mongo-connection.js
+++ server/src/util/database/mongo-connection.js
@@ -1,17 +1,37 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient("mongodb://root:root@localhost:27017/chat_database?authSource=admin");
-let userCollection;
+class Database {
+	#client;
+	#userCollection;
 
-try {
-	await client.connect();
+	constructor() {
+		const url = "mongodb://root:root@localhost:27017/chat_database?authSource=admin";
+		this.#client = new MongoClient(url);
+		this.#userCollection = null;
+	}
 
-	const db = client.db();
-	userCollection = db.collection("user");
+	async connect() {
+		try {
+			await this.#client.connect();
+			const db = this.#client.db();
+			this.#userCollection = db.collection("user");
+			console.log("Connection => Database connection established!");
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
 
-	console.log("Connection => Database connection established!");
-} catch (error) {
-	throw new Error(error);
+	getUserCollection() {
+		if (!this.#userCollection) {
+			throw new Error("Database connection not established. Call connect() first.");
+		}
+		return this.#userCollection;
+	}
+
+	async close() {
+		await this.#client.close();
+		console.log("Connection => Database connection closed.");
+	}
 }
 
-export { userCollection };
+export default Database;
