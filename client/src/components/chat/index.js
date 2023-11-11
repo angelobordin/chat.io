58f8a67320d18a7bb5819999da2384b25b965fd5
AngeloBordin <angelombordin@gmail.com>
2023-11-11 16:49:52 -0300

    implements message evetn with socket.io

--- client/src/components/chat/index.js
+++ client/src/components/chat/index.js
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "./chat.css";
+
+const socket = io("http://localhost:8080");
+socket.on("connect", () => console.log(`[IO] Connect => New connection established`));
 
 const Chat = ({ selectedContact }) => {
 	const [messages, setMessages] = useState([]);
 	const [newMessage, setNewMessage] = useState("");
 
-	const handleSendMessage = () => {
-		// Send the newMessage to the backend
-		// Example: fetch('/api/sendMessage', { method: 'POST', body: JSON.stringify({ to: selectedContact, message: newMessage }) });
-		// Replace the above fetch with your actual API call
+	useEffect(() => {
+		const handlerNewMessage = (newMessage) => setMessages([...messages, newMessage]);
 
-		// For testing purposes, let's simulate adding a message to the conversation
-		const updatedMessages = [...messages, { sender: "Me", text: newMessage }];
-		setMessages(updatedMessages);
-		setNewMessage("");
+		socket.on("chat.message", handlerNewMessage);
+		return () => socket.off("chat.message", handlerNewMessage);
+	}, [messages]);
+
+	const handlerSendMessage = () => {
+		if (newMessage.trim()) {
+			socket.emit("chat.message", { sender: "Me", text: newMessage });
+			setNewMessage("");
+		}
 	};
 
 	return (
@@ -26,13 +34,23 @@ const Chat = ({ selectedContact }) => {
 							</div>
 						))}
 					</div>
-					<div className="input-section">
-						<input type="text" placeholder="Type a message..." value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
-						<button onClick={handleSendMessage}>Send</button>
+					<div className="form">
+						<input
+							type="text"
+							placeholder="Type a message..."
+							value={newMessage}
+							onChange={(e) => setNewMessage(e.target.value)}
+							onKeyDown={(e) => e.key === "Enter" && handlerSendMessage()}
+						/>
+						<button onClick={handlerSendMessage} type="button">
+							Send
+						</button>
 					</div>
 				</>
 			) : (
-				<p>Select a contact to start a conversation</p>
+				<div className="layout-message">
+					<p className="message">Select a contact to start a conversation</p>
+				</div>
 			)}
 		</div>
 	);
