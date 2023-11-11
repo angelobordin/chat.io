7d12b15ac5c87b524eb87dbaa8dfd0ae05396432
AngeloBordin <angelombordin@gmail.com>
2023-11-11 08:07:03 -0300

    implements body parser

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,17 @@
 import cors from "cors";
 import express from "express";
+import bodyParser from "body-parser";
+import UserRoutes from "./routes/user-routes.js";
+import "./util/database/mongo-connection.js";
 
 class Server {
 	#server;
 	#port;
 
+	/**
+	 *
+	 * @param {number} _port
+	 */
 	constructor(_port) {
 		this.#server = new express();
 		this.#port = _port;
@@ -17,6 +24,7 @@ class Server {
 		console.log("Initializing Middlewares...");
 		this.#server.use(express.json());
 		this.#server.use(cors());
+		this.#server.use(bodyParser.json());
 		// this.#server.use(fileMiddleware);
 		// this.#server.use(decodeToken);
 		console.log("Complete!");
@@ -24,7 +32,7 @@ class Server {
 
 	initRouteBase() {
 		console.log("Initializing routes...");
-		// this.#server.use(CustomerRouter);
+		this.#server.use(UserRoutes);
 		console.log("Complete!");
 	}
 
