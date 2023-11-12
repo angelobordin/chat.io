241ccf45d00be668de3cefe34bf5d8ecb8e3da77
AngeloBordin <angelombordin@gmail.com>
2023-11-12 19:47:48 -0300

    create routine in front and backend to notify user when receive message

--- client/src/contexts/socket.js
+++ client/src/contexts/socket.js
@@ -1,11 +1,24 @@
 import { createContext, useEffect } from "react";
 import io from "socket.io-client";
 import { localhostURL } from "../util/api/routes";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
 
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
 	const socket = io(localhostURL);
+
+	useEffect(() => {
+		socket.on("message notification", (messageData) => {
+			const user = JSON.parse(localStorage.getItem("user_data"));
+			if (messageData.receiver.id === user.id) {
+				const { sender } = messageData;
+
+				toast.success(`${sender.name} te enviou uma mensagem!`);
+			}
+		});
+	});
 
 	useEffect(() => {
 		return () => {
--- server/src/socket.js
+++ server/src/socket.js
@@ -29,9 +29,15 @@ class SocketServer {
 			});
 
 			socket.on("chat.message", async (message) => {
-				await MessageService.registerMessage(message);
+				const chatData = await MessageService.registerMessage(message);
+				const users = chatData.data.userNames;
 
 				socket.to(message.room).emit("chat.message", message);
+
+				this.#io.emit("message notification", {
+					sender: users.filter((user) => user.id === message.user)[0],
+					receiver: users.filter((user) => user.id !== message.user)[0],
+				});
 			});
 
 			socket.on("login", (userId) => {
